f3af47557fcec0d292c17f0555cb12626d27fe97
Luke Bertram <lukebertram@gmail.com>
2017-11-24 20:52:21 -0800

    convert javascript to jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>7G</h3>
+    <h3>Dirty Charles and Cool Dennis</h3>
+    <p><em>Firebase literally on fire</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
