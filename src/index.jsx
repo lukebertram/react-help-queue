432cde4e362c0dad898e5d309d5b8b54ca0d8d07
Luke Bertram <lukebertram@gmail.com>
2017-11-25 18:06:14 -0800

    complete basic webpack configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
     <h1>Help Queue</h1>
     <h3>7G</h3>
     <h3>Dirty Charles and Cool Dennis</h3>
-    <p><em>Firebase literally on fire</em></p>
+    <p><em>Firebase is literally on fire</em></p>
   </div>,
   document.getElementById('react-app-root')
 );
