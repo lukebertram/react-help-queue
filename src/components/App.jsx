dcec3d7b6b04bfd3aa0be284c3dff1b1b8ceed71
Luke Bertram <lukebertram@gmail.com>
2017-11-26 16:11:25 -0800

    move App component code to src/compoonents/App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(props){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>7G</h3>
+      <h3>Dirty Charleston and Cool Dennis</h3>
+      <p><em>Firebase is literally on fire</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>7G</h3>
-    <h3>Dirty Charles and Cool Dennis</h3>
-    <p><em>Firebase is literally on fire</em></p>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
