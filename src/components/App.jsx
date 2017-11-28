63bccc37226a08626c36eb1401c0af290402c34e
Luke Bertram <lukebertram@gmail.com>
2017-11-27 19:05:33 -0800

    decompose app.jsx into separate react components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(props){
   return (
     <div>
-      <h1>Help Queue</h1>
-      <h3>7G</h3>
-      <h3>Dirty Charleston and Cool Dennis</h3>
-      <p><em>Firebase is literally on fire</em></p>
-      <hr/>
+      <Header/>
+      <TicketList />
     </div>
   );
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ticket(props){
+  return(
+    <div>
+      <h3>7G - Dirty Charles & Cool Dennis</h3>
+      <p><em>Firebase is literally on fire</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
