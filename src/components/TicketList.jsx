57a3d25f0b4b7d4d6804a67ed16798779769223d
Luke Bertram <lukebertram@gmail.com>
2018-02-06 14:24:30 -0800

    use loop to iterate over tickets in ticketlist, add key rule to linter

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+const masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase literally on fire.'
+  },
+  {
+    names: 'Jimbo and Kearney',
+    location: 'Attitud Adjustment and Reprogramming Center',
+    issue: 'Blood is lost'
+  },
+  {
+    names: 'Dirty Charles and Cool Dennis',
+    location: 'Mars Base 7',
+    issue: 'Out of Sweet\'n\'Low'
+  }
+];
+
 function TicketList(){
   return(
-    <Ticket
-      location="7G"
-      names="Dirty Charles and Cool Dennis"
-      issue="Firebase is literally on fire"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
   );
 }
 
