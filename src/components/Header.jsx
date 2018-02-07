573c0a7de4da5ae1540d83ac6e67773e7bb607df
Luke Bertram <lukebertram@gmail.com>
2018-02-06 16:20:17 -0800

    add support for images in react, image to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import reactLogo from '../assets/images/react-logo.svg';
 
 function Header(){
+  let logoStyle ={
+    width: 500
+  };
   return(
     <div>
+      <img src={reactLogo} style={logoStyle} />
       <h1>Hot modules, dawg!</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
