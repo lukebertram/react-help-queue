56f60937de5c1d59aababb46ff92cf920e7f2d0b
Luke Bertram <lukebertram@gmail.com>
2018-02-11 14:46:30 -0800

    and ConfirmationQuestions component and conditional rendering in NewTicketControl

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tanhauser Gate. All those moments will be lost in time, like tears in rain. Time to die.
+function ConfirmationQuestions(){
+  return (
+    <div>
+      <p>Have you seen things we people wouldn't believe?</p>
+      <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestions;
