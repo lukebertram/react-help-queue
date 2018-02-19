db5fa3f50de7afb951e58f2dff289bf75551f5fc
Luke Bertram <lukebertram@gmail.com>
2018-02-19 14:01:49 -0800

    add ADD_TICKET case to ticket-list-reducer with testing

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,39 @@
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
 
-  test('Temporary dummy test: two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+  let action;
+  const sampleTicketData = {
+    names : 'Roy and Deckard',
+    location: 'Rooftop',
+    issue: `ES6 syntax is not recognized in Jest tests`,
+    timeOpen : 1500000000000,
+    id: 0
+  };
+
+  test('Should return default state if no actionn type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
   });
 
 });
